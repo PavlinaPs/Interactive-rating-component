aef22e2a8af07b90c3b1827e20ccb3ef0d519841
PPS <pavlina.psarsky@gmail.com>
2022-03-22 08:00:42 +0100

    all working, before radio

---
+++ script.js
@@ -0,0 +1,21 @@
+const numbers = document.querySelectorAll(".numbers");
+const numberSelected = document.getElementById('selected-number')
+const ratingDisplay = document.getElementById('rating')
+const thankYouDisplay = document.getElementById('thank-you')
+
+let numberId
+numbers.forEach(number => number.addEventListener('click', (e) => {
+    numberId = e.target.id;
+    number.setAttribute('aria-selected', 'true');
+    numberSelected.innerText = numberId;
+    //console.log(numberSelected)
+}));
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', displayThankYou)
+
+function displayThankYou() {
+    numbers.forEach(number => number.setAttribute('aria-selected', 'false'));
+    ratingDisplay.setAttribute('data-visible', 'false')
+    thankYouDisplay.setAttribute('data-visible', 'true')
+}
