7934f5a68788b3e7ae1036a38abe12885037f1f3
PPS <pavlina.psarsky@gmail.com>
2022-03-22 14:17:38 +0100

    last touches

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 const numbers = document.querySelectorAll(".numbers");
-const numberSelected = document.getElementById('selected-number')
-const ratingDisplay = document.getElementById('rating')
-const thankYouDisplay = document.getElementById('thank-you')
+const numberSelected = document.getElementById('selected-number');
+const ratingDisplay = document.getElementById('rating');
+const thankYouDisplay = document.getElementById('thank-you');
+const submit = document.getElementById('submit');
+let choice = [];
 
 function selectRating(){
-    let numberId
+    let numberId;
     numbers.forEach(number => number.addEventListener('click', (e) => {
-        numbers.forEach(item => item.setAttribute('aria-selected', 'false'))
+        numbers.forEach(item => item.setAttribute('aria-selected', 'false'));
         numberId = e.target.id;
         number.setAttribute('aria-selected', 'true');
+        choice.push(numberId);
         numberSelected.innerText = numberId;
     }));
-    
 }
 selectRating();
 
-
-const submit = document.getElementById('submit');
-submit.addEventListener('click', displayThankYou)
-
+function submitChoice() {
+    submit.addEventListener('click', () => {
+        if (choice.length === 0) {
+            alert('Please choose rating');
+        } else {
+            displayThankYou();
+        }
+    })
+}
+submitChoice();
 
 function displayThankYou() {
-    
-    ratingDisplay.setAttribute('aria-hidden', 'true')
-    thankYouDisplay.setAttribute('aria-hidden', 'false')
+    ratingDisplay.setAttribute('aria-hidden', 'true');
+    thankYouDisplay.setAttribute('aria-hidden', 'false');
 }
 
 
